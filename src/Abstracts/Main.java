df12c8a658c4c69b10a676c6d3416c6d10d1bbc6
Fylyheng <fylyheng.phally@gmail.com>
2020-01-20 21:32:19 +0700

    update project

--- src/Abstracts/Main.java
+++ src/Abstracts/Main.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public class Main implements Serializable {
     public static void main(String[] args) {
+        //this is comment
 
         /**
          * abstract
