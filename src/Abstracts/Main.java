dcfd6e3541492c0aa898415b0b140e60e3a6228f
Fylyheng <fylyheng.phally@gmail.com>
2020-01-21 08:42:38 +0700

    clean code

--- src/Abstracts/Main.java
+++ src/Abstracts/Main.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 public class Main implements Serializable {
     public static void main(String[] args) {
-        //this is comment
 
         /**
          * abstract
